b962705ec60fc9751dfa12562c7f2860cfad96ea
Woody See <woodyseesk@gmail.com>
2021-11-21 18:03:42 +0800

    Initialise project

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <TodoList />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TodoList from "@/components/TodoList.vue";
+
+export default {
+  name: "Home",
+  components: {
+    TodoList,
+  },
+};
+</script>
